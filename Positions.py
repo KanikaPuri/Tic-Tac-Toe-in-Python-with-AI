5c861eec2fa46460c1339d6a9bd39da8b9224ff5
Kanika Puri <purikanikasingh@gmail.com>
2018-07-30 04:21:49 -0400

    New Upload

---
+++ Positions.py
@@ -0,0 +1,19 @@
+#Contains several tic tac toe positions to test 
+x_move_1 = ['X',None,None,
+            'X',None,None,
+            'X',None,None]
+x_move_2 = [None,'X',None,
+            None,None,None,
+            None,None,None]
+x_move_3 =  [None,None,None,
+            None,'X',None,
+            None,None,None]
+x_wins_1 = ['X','O',None,
+            None,None,None,
+            None,None,None]
+x_wins_2 = ['X',None,'O',
+            None,None,None,
+            None,None,None]
+o_wins_1 = [None, 'X', None,
+            None, 'O',None,
+            None, 'X',None]
